ac9dee3b86c4a5df04028a58eb87e8e1d025a2e6
Lattapon Yodsuwan <i@clozed2u.com>
2014-07-27 19:44:25 +0700

    Edit front page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
 $(document).ready(function() {
+    $('#start').css({
+        left: ($(window).width() - $('#start').outerWidth()) / 2,
+        top: ($(window).height() - $('#start').outerHeight()) / 2
+    });
+    $('#three').css({
+        left: ($(window).width() - $('#three').outerWidth()) / 2,
+        top: ($(window).height() - $('#three').outerHeight()) / 2
+    });
+    $('#two').css({
+        left: ($(window).width() - $('#two').outerWidth()) / 2,
+        top: ($(window).height() - $('#two').outerHeight()) / 2
+    });
+    $('#one').css({
+        left: ($(window).width() - $('#one').outerWidth()) / 2,
+        top: ($(window).height() - $('#one').outerHeight()) / 2
+    });
+    $('#content').css({
+        left: ($(window).width() - $('#content').outerWidth()) / 2,
+        top: ($(window).height() - $('#content').outerHeight()) / 2
+    });
+    $('#stat').css({
+        left: ($(window).width() - $('#stat').outerWidth()) / 2,
+        top: ($(window).height() - $('#stat').outerHeight()) / 2
+    });
+
     $(document).unbind('keydown').bind('keydown', function(e) {
         var doPrevent = false;
 
@@ -105,14 +130,14 @@ $(document).ready(function() {
                 var periodtime = ((endtime - starttime) / 1000).toFixed(1);
                 var wpm = ((60 / periodtime) * allWords.length).toFixed(0);
                 var accuracy = ((correctChars / totalChars) * 100).toFixed(0);
-                var expectwpm = 35;
+                var expectwpm = 30;
                 var expectAccuracy = 80;
                 console.log("correct " + correctChars);
                 console.log("wrong " + wrongChars);
                 console.log("total " + totalChars);
                 console.log("periodtime " + periodtime);
                 console.log("wpm " + wpm);
-                $('#stat').prepend("Accuracy: " + accuracy + "%<br>WPM: " + wpm + "<br>Time: " + periodtime + "s");
+                $('#stat').prepend("ความแม่นยำ: " + accuracy + "%<br>จำนวนคำต่อนาที: " + wpm + "<br>เวลาที่ใช้: " + periodtime + " วินาที");
                 $('#content').fadeOut('fast', function() {
                     $('#stat').show();
                     if (wpm > expectwpm && accuracy > expectAccuracy) {
